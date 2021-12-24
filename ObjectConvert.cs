9367697901917dc3e082a45cafec855986f40075
K. Yasin <58375357+kYasinAblay@users.noreply.github.com>
2021-12-24 15:25:49 +0300

    okey
    ok

--- ObjectConvert.cs
+++ ObjectConvert.cs
@@ -1,19 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
+
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace ReflectionAutoMapper
 {
     internal static class ObjectConvert
     {
-        public static ModelResult Convert<m, ModelResult>(m Model) where ModelResult:class, new()
+        public static ModelResult Convert<m, ModelResult>(m Model) where ModelResult : class, new()
         {
             ModelResult result = new ModelResult();
             foreach (var item in typeof(m).GetProperties().ToList())
             {
-                var property = typeof(ModelResult).GetProperties(item.Name);
+                var property = typeof(ModelResult).GetProperty(item.Name);
                 if (property != null)
                 {
                     property.SetValue(result, item.GetValue(Model));
